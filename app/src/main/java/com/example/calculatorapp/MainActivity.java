fba5683807d26110c7edc9aa934519957a34531f
Antti Romppainen <antti.romppainen@hotmail.com>
2026-03-05 09:22:42 +0200

    Create sum operation

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.calculatorapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText userInputField1;
+    private EditText userInputField2;
+    private TextView outputField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        userInputField1 = findViewById(R.id.inputNumOne);
+        userInputField2 = findViewById(R.id.inputNumTwo);
+        outputField = findViewById(R.id.numOutput);
     }
+
+    public void sumTwoNumbers(View view) {
+        String input1 = userInputField1.getText().toString();
+        String input2 = userInputField2.getText().toString();
+
+        if (!input1.isEmpty() && !input2.isEmpty()) {
+            int value1 = Integer.parseInt(input1);
+            int value2 = Integer.parseInt(input2);
+            outputField.setText(String.valueOf(value1 + value2));
+        } else {
+            Toast.makeText(this, "One or two number fields currently empty", Toast.LENGTH_LONG).show();
+            outputField.setText("");
+        }
+    }
+
 }
